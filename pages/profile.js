76b7830d95a5446356fbcdaf66d19682acc57a64
suminlee <suminpixel@gmail.com>
2020-06-08 23:58:16 +0900

    next router test

---
+++ pages/profile.js
@@ -0,0 +1,9 @@
+const Profile = () => {
+    return (
+        <div>
+            내 프로필
+        </div>
+    )
+}
+
+export default Profile;
---
+++ pages/signup.js
@@ -0,0 +1,9 @@
+const Signup = () => {
+    return (
+        <div>
+            회원가입
+        </div>
+    )
+}
+
+export default Signup;
